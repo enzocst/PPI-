29d5b4f923b3c2723483c2e8e28e8b1afcfd2d1d
enzocst <enzoc432@gmail.com>
2023-04-17 14:28:49 -0300

    main

--- atividade2/questao3.js
+++ atividade2/questao3.js
@@ -1,9 +1,9 @@
 let data1 = prompt("Coloque a primeira data");
 let data2 = prompt("Coloque a segunda data");
 
-let valor1 = data1.split(("/")[2]);
-let valor2 = data2.split(("/")[2]);
+let valor1 = +data1.split("/")[2];
+let valor2 = +data2.split("/")[2];
 
-let diferença = data2 - data1;
+let diferença = valor2 - valor1;
 
 console.log(diferença);
--- atividade2/questao5.js
+++ atividade2/questao5.js
@@ -3,7 +3,7 @@ let valorpago = prompt("Digite o valor pago por cada hora");
 
 if (horastrab > 40){
     let extras = horastrab - 40;
-    pagamento = (40 * valorpago) + (extras)
+    pagamento = (40 * valorpago * 1.5) + (extras);
 } else {
     pagamento = horastrab * valorpago;
 }
