c13e7497190ee805eefbff481e00f7cf064879c7
enzocst <enzoc432@gmail.com>
2023-05-09 13:36:17 -0300

    main

--- atividade4/qstao2.js
+++ atividade4/qstao2.js
@@ -1,9 +1,23 @@
-let digitos = prompt('9 PRIMEIROS DIGITOS DO CPF!!!!!');
-let mult = 10;
-let s1 = 0;
-let pos = 0;
-while(mult <=2){
-    s1 += +digitos[pos] * mult;
-    mult--;
-    pos++;
+
+let cpf = prompt("Digite os 9 primeiros algarismos do CPF:");
+
+
+let soma1 = 0;
+for (let i = 0; i < 9; i++) {
+  soma1 += parseInt(cpf.charAt(i)) * (10 - i);
 }
+let resto1 = soma1 % 11;
+let d1 = (resto1 < 2) ? 0 : 11 - resto1;
+
+
+cpf += d1;
+
+
+let soma2 = 0;
+for (let i = 0; i < 10; i++) {
+  soma2 += parseInt(cpf.charAt(i)) * (11 - i);
+}
+let resto2 = soma2 % 11;
+let d2 = (resto2 < 2) ? 0 : 11 - resto2;
+
+alert("Os verificadores são: " + d1 + d2);
