8c70c970cc283e9232842e8410275d400a1bb2cd
enzocst <enzoc432@gmail.com>
2023-05-22 14:16:48 -0300

    main

---
+++ exemplos de sala/mat.js
@@ -0,0 +1,32 @@
+/**
+ * Recebe 3 números e diz se esses números formam um
+ * triangulo retangulo.
+ */
+// function triangulo(hp, c1, c2) {
+//     return hp ** 2 == c1 ** 2 + c2 ** 2;;
+// }
+
+// console.log(triangulo(5, 4, 3));
+// console.log(triangulo(5, 4, 3));
+// let hp = 5;
+// let c1 = 3;
+// let c2 = 3;
+// if (triangulo(hp, c1, c2)) {
+//     document.write('<p>é um triângulo</p>');
+// } else {
+//     document.write('<p>Não é um triângulo</p>');
+// }
+
+// console.log(fatorial(8));
+
+function par(n){
+    return n % 2 == 0;
+}
+
+
+function fatorial(n) {
+    let i = n;
+    while (i >= 2) {
+
+    }
+}
