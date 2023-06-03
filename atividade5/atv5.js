5220c120bb7100d16346080b40efb873b90d23a2
enzocst <enzoc432@gmail.com>
2023-06-02 22:53:08 -0300

    main

---
+++ atividade5/atv5.js
@@ -0,0 +1,21 @@
+function principal() {
+    // Gera um número aleatório entre 1 e 20
+    return Math.floor(Math.random() * 20) + 1;
+  }
+
+  function comparar(palpite, gerado) {
+    if (palpite === gerado) {
+      return 'Parabéns! Você acertou o número.';
+    } else if (palpite < gerado) {
+      return 'Tente um número maior!';
+    } else {
+      return 'Tente um número menor!';
+    }
+  }
+
+  function gerarNumero() {
+    var gerado = principal();
+    var palpite = parseInt(document.getElementById('palpite').value);
+    var resultado = comparar(palpite, gerado);
+    alert(resultado);
+  }
