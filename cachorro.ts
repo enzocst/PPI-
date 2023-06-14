804b9cb908c59f72b3cae0e003b0092cfa3abed3
enzocst <enzoc432@gmail.com>
2023-06-14 14:01:16 -0300

    main

---
+++ cachorro.ts
@@ -0,0 +1,11 @@
+export class Cachorro {
+    nome: string;
+    raça: string;
+    idade: number;
+
+    constructor(){
+        this.nome = '';
+        this.raça = 'Salsicha';
+        this.idade = 0;
+    }
+}
