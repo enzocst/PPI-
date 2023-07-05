ef90b33ab071d377412346387a4565f311a42285
enzocst <enzoc432@gmail.com>
2023-07-05 14:20:27 -0300

    main

---
+++ 2° Bim/Atividade1/q2.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Contato = void 0;
+class Contato {
+    constructor(nm, eml, tele) {
+        this.nome = nm;
+        this.email = eml;
+        this.telefone = tele;
+    }
+}
+exports.Contato = Contato;
---
+++ 2° Bim/Atividade1/q2.ts
@@ -0,0 +1,12 @@
+export class Contato{
+    private nome: string;
+    private email: string;
+    private telefone: number;
+
+constructor(nm: string, eml: string, tele: number){
+    this.nome = nm;
+    this.email = eml;
+    this.telefone = tele;
+}
+
+}
