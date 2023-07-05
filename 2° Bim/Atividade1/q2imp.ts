481ec7dc79cd80ee7438464ada4751ae1e2dc934
enzocst <enzoc432@gmail.com>
2023-07-05 15:10:03 -0300

    main

--- 2° Bim/Atividade1/q2imp.ts
+++ 2° Bim/Atividade1/q2imp.ts
@@ -2,7 +2,13 @@ import { Contato } from "./q2";
 
 let cliente = new Contato('Jaozin','jaozinbalaseca@gmail.com',986565632);
 console.log(cliente);
+cliente.alterarnome('Jaozão');
+console.log(cliente.obternome());
+cliente.alteraremail('jaozabalaseca@gmail.com');
+console.log(cliente.obteremail());
+cliente.alterartelefone(954652134);
+console.log(cliente.obtertelefone());
 
-
+console.log(cliente);
 
 
