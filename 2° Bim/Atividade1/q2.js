6be616bfbb5685be0c8a02139d59efeefb801d63
enzocst <enzoc432@gmail.com>
2023-07-05 14:30:09 -0300

    main

--- 2° Bim/Atividade1/q2.js
+++ 2° Bim/Atividade1/q2.js
@@ -3,9 +3,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Contato = void 0;
 class Contato {
     constructor(nm, eml, tele) {
-        this.nome = nm;
-        this.email = eml;
-        this.telefone = tele;
+        this.alterarnome(nm);
+        this.alteraremail(eml);
+        this.alterartelefone(tele);
+    }
+    alterarnome(n) {
+        this.nome;
+        return true;
+        return false;
+    }
+    obternome() {
+        return this.nome;
+    }
+    alteraremail(n) {
+        this.email;
+        return true;
+        return false;
+    }
+    obteremail() {
+        return this.email;
+    }
+    alterartelefone(n) {
+        if (n >= 0 && n <= 100) {
+            this.telefone = n;
+            return true;
+        }
+        return false;
+    }
+    obtertelefone() {
+        return this.telefone;
     }
 }
 exports.Contato = Contato;
--- 2° Bim/Atividade1/q2.ts
+++ 2° Bim/Atividade1/q2.ts
@@ -1,12 +1,44 @@
-export class Contato{
+export class Contato {
     private nome: string;
     private email: string;
     private telefone: number;
 
-constructor(nm: string, eml: string, tele: number){
-    this.nome = nm;
-    this.email = eml;
-    this.telefone = tele;
-}
+    constructor(nm: string, eml: string, tele: number) {
+        this.alterarnome(nm);
+        this.alteraremail(eml);
+        this.alterartelefone(tele);
+    }
+    alterarnome(n: string): boolean {
 
+        this.nome
+        return true;
+        return false;
+    }
+
+    obternome(): string {
+        return this.nome;
+    }
+
+    alteraremail(n: string): boolean {
+        this.email
+        return true;
+        return false;
+
+    }
+    obteremail(): string {
+        return this.email;
+    }
+
+    alterartelefone(n: number): boolean {
+        if (n >= 0 && n <= 100) {
+            this.telefone = n;
+            return true;
+
+        }
+        return false;
+    }
+
+    obtertelefone(): number {
+        return this.telefone;
+    }
 }
