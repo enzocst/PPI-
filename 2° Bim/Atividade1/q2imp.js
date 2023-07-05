db080728ddb57129083610f934b7dec1076d8bbe
enzocst <enzoc432@gmail.com>
2023-07-05 15:00:40 -0300

    main

--- 2° Bim/Atividade1/q2imp.js
+++ 2° Bim/Atividade1/q2imp.js
@@ -1,3 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let;
+const q2_1 = require("./q2");
+let cliente = new q2_1.Contato('Jaozin', 'jaozinbalaseca@gmail.com', 986565632);
+console.log(cliente);
--- 2° Bim/Atividade1/q2imp.ts
+++ 2° Bim/Atividade1/q2imp.ts
@@ -1,3 +1,8 @@
 import { Contato } from "./q2";
 
-let 
+let cliente = new Contato('Jaozin','jaozinbalaseca@gmail.com',986565632);
+console.log(cliente);
+
+
+
+
