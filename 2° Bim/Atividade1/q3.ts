c982c0bc332058aa6e89046c465d0032efaac44e
enzocst <enzoc432@gmail.com>
2023-07-10 11:09:10 -0300

    main

--- 2° Bim/Atividade1/q3.ts
+++ 2° Bim/Atividade1/q3.ts
@@ -1,3 +1,77 @@
-export class Televisao{
-    
+class Televisao {
+    private volume: number;
+    private canal: number;
+
+    constructor() {
+        this.volume = 0;
+        this.canal = 0;
+    }
+
+    public alterarVolume(volume: number) {
+        if (volume >= 0 && volume <= 100) {
+            this.volume = volume;
+        }
+    }
+
+    public obterVolume(): number {
+        return this.volume;
+    }
+
+    public alterarCanal(canal: number) {
+        if (canal >= 0 && canal <= 800) {
+            this.canal = canal;
+        }
+    }
+
+    public obterCanal(): number {
+        return this.canal;
+    }
+}
+
+class ControleRemoto {
+    private televisao: Televisao;
+
+    constructor(televisao: Televisao) {
+        this.televisao = televisao;
+    }
+
+    public aumentarVolume() {
+        const volumeAtual = this.televisao.obterVolume();
+        if (volumeAtual < 100) {
+            this.televisao.alterarVolume(volumeAtual + 1);
+        }
+    }
+
+    public diminuirVolume() {
+        const volumeAtual = this.televisao.obterVolume();
+        if (volumeAtual > 0) {
+            this.televisao.alterarVolume(volumeAtual - 1);
+        }
+    }
+
+    public consultarVolume(): number {
+        return this.televisao.obterVolume();
+    }
+
+    public aumentarCanal() {
+        const canalAtual = this.televisao.obterCanal();
+        if (canalAtual < 800) {
+            this.televisao.alterarCanal(canalAtual + 1);
+        }
+    }
+
+    public diminuirCanal() {
+        const canalAtual = this.televisao.obterCanal();
+        if (canalAtual > 0) {
+            this.televisao.alterarCanal(canalAtual - 1);
+        }
+    }
+
+    public trocarCanal(canal: number) {
+        this.televisao.alterarCanal(canal);
+    }
+
+    public consultarCanal(): number {
+        return this.televisao.obterCanal();
+    }
 }
---
+++ 2° Bim/Atividade1/q3imp.ts
@@ -0,0 +1,13 @@
+
+
+let televisao = new Televisao();
+let controleRemoto = new ControleRemoto(televisao);
+
+controleRemoto.aumentarVolume();
+console.log(controleRemoto.consultarVolume());
+
+controleRemoto.aumentarCanal();
+console.log(controleRemoto.consultarCanal());
+
+controleRemoto.trocarCanal(5);
+console.log(controleRemoto.consultarCanal());
