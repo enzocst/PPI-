b73554891a63c0712c92c6a384e62f914916cb07
enzocst <enzoc432@gmail.com>
2023-08-28 13:53:25 -0300

    main

--- 2° Bim/avaliação/avaliacao.ts
+++ 2° Bim/avaliação/avaliacao.ts
@@ -1,49 +1,90 @@
 export class Data {
-    dia: number;
-    mes: number;
-    ano: number;
+    private dia: number;
+    private mes: number;
+    private ano: number;
 
-    constructor(dia: number, mes: number, ano: number) {
-        this.dia = dia;
-        this.mes = mes;
-        this.ano = ano;
-
+    constructor(dd: number,
+        mm: number,
+        aa: number) {
+        this.modificarDia(dd);
+        this.modificarMes(mm);
+        this.modificarAno(aa);
     }
 
-    modificardia(dia: number) {
-        this.dia = dia;
+    modificarDia(dia: number) {
+        if (dia >= 1 && dia <= 31) {
+            this.dia = dia;
+        } else {
+            this.dia = 1;
+        }
     }
 
-    obterdia(): number {
+    obterDia(): number {
         return this.dia;
+    }
+
+    modificarMes(mm: number) {
+        if (mm >= 1 && mm <= 12) {
+            this.mes = mm;
+        } else {
+            this.mes = 1;
+        }
+    }
+
+    obterMes(): number {
+        return this.mes;
+    }
+
+    modificarAno(aa: number) {
+        const anoAtual = new Date().getFullYear();
+        if (aa >= anoAtual) {
+            this.ano = aa;
+        } else {
+            this.ano = anoAtual;
+        }
+    }
+
+    obterAno(): number {
+        return this.ano;
+    }
+
+    obterData(): string {
+        return `${this.dia}/${this.mes}/${this.ano}`;
     }
 }
 
 export class Voo {
     data: Data;
     numero: number;
-    reservas: boolean;
+    reservas: boolean[];
 
-    constructor(data: Data, numero: number) {
+    constructor(data: Data, num: number) {
         this.data = data;
-        this.numero = numero;
-        this.reservas = false;
+        this.numero = num;
+        this.reservas = [false, false, false, false, false, false, false, false];
     }
 
-    ocupado(assento: number): void{
-        if(assento > 7){
-            this.reservas = true 
-        } else {
-            false
+    ocupado(num: number) {
+        if (num >= 0 && num < this.reservas.lenght) {
+            return this.reservas[num];
         }
-
-    }
-    
-    quantidadeocupados(): number{
-        return this.ocupado;
+        return false
     }
 
-    reservar(numero: number): boolean{
+    quantidadeocupados(): number {
+        let quant = 0;
+        for (let i = 0; i < this.reservas.lenght; i++) {
+            if (this.reservas[i]) {
+                quant++;
+            }
+        }
+    }
 
+    reservar(num: number) {
+        if (!this.ocupado(num)) {
+            this.reservas[num] = true;
+            return true;
+        }
+        return false
     }
 }
--- 2° Bim/avaliação/avaliacaoimp.ts
+++ 2° Bim/avaliação/avaliacaoimp.ts
@@ -1,7 +1,7 @@
 import { Data, Voo } from "./avaliacao";
 
-let data = new Data (31,7,2023);
-let voo = new Voo (31,7,2023,5);
+let voo = new Voo (1, new Data(5, 11, 2023));
+let voo2 = new Voo (2, new Data(12, 9, 2024));
 
 console.log(data);
 console.log(voo);
